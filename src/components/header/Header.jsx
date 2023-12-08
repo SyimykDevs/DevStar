a10c493871bde7b8e65d2037d029e1966e1b9238
sYimyk3 <syim7k.dev@gmail.com>
2023-12-08 10:31:07 +0600

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <header className="header">
+            <div className="container">
+                <div className="header-wrapper">
+                    <div className="header-logo">
+                        
+                    </div>
+                </div>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default Header
