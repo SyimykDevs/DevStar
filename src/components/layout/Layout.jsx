dfbbf47c09da5545a5ccfbffe2bdf2ff884339e0
sYimyk3 <syim7k.dev@gmail.com>
2023-12-08 11:31:11 +0600

    router

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+
+import Header from '../header/Header'
+import { Outlet } from 'react-router-dom'
+import  Footer  from '../footer/Footer'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
   )
 }
 
---
+++ src/pages/Comands.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Comands = () => {
+  return (
+    <div>Comands</div>
+  )
+}
+
+export default Comands
